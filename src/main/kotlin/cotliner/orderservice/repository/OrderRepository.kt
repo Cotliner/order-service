9016230b7fe61f635cdac37f5ac759a27867c33a
Cotliner <carthymariejoseph@gmail.com>
2022-01-24 22:59:39 +0100

    Finalize exercice

--- src/main/kotlin/cotliner/orderservice/repository/OrderRepository.kt
+++ src/main/kotlin/cotliner/orderservice/repository/OrderRepository.kt
@@ -1,13 +1,11 @@
 package cotliner.orderservice.repository
 
-import cotliner.serversideevent.document.order.Order
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
+import cotliner.orderservice.document.order.Order
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
-@Repository interface OrderRepository: MongoRepository<Order, UUID> {
+@Repository interface OrderRepository: MongoRepository<Order, UUID> { /* TODO-6: MAKE REPOSITORY REACTIVE */
   /* SEARCH QUERIES */
-  fun findAllByPriceBetween(startPrice: Number, endPrice: Number, pageable: Pageable): Page<Order>
+  // fun findAllByPriceBetween(startPrice: Number, endPrice: Number, pageable: Pageable): Page<Order>
 }
--- src/main/kotlin/cotliner/orderservice/wr/OrderResource.kt
+++ src/main/kotlin/cotliner/orderservice/wr/OrderResource.kt
@@ -1,16 +1,11 @@
 package cotliner.orderservice.wr
 
-import cotliner.orderservice.commons.SearchParam
 import cotliner.orderservice.service.OrderService
 import cotliner.orderservice.wr.OrderResource.Companion.URI
-import cotliner.serversideevent.document.order.Order
-import cotliner.serversideevent.document.order.dto.OrderInputDto
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageRequest
+import cotliner.orderservice.document.order.Order
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
-import java.util.*
 
 @RequestMapping(value = [ URI ], produces = [MediaType.APPLICATION_JSON_VALUE] )
 @RestController
@@ -22,31 +17,31 @@ class OrderResource(
 
   @ResponseStatus(HttpStatus.OK)
   @GetMapping
-  fun getAll(): List<Order> = orderService.findAll()
+  fun getAll(): List<Order> = orderService.findAll() /* TODO-8: CHANGE RETURN TYPE */
 
-  @ResponseStatus(HttpStatus.CREATED)
-  @PostMapping
-  fun create(@RequestBody orderInput: OrderInputDto): Order = orderService.create(orderInput)
-
-  @ResponseStatus(HttpStatus.OK)
-  @PutMapping("{id}")
-  fun update(
-    @PathVariable id: UUID,
-    @RequestBody orderInput: OrderInputDto
-  ): Order = orderService.update(id, orderInput)
-
-  @ResponseStatus(HttpStatus.OK)
-  @PostMapping("search")
-  fun search(
-    @RequestBody param: SearchParam,
-    @RequestParam page: Number,
-    @RequestParam size: Number
-  ): Page<Order> = orderService.search(
-    param,
-    PageRequest.of(page.toInt(), size.toInt())
-  )
-
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  @DeleteMapping("{id}")
-  fun delete(@PathVariable id: UUID): Unit = orderService.delete(id)
+  //  @ResponseStatus(HttpStatus.CREATED)
+  //  @PostMapping
+  //  fun create(@RequestBody orderInput: OrderInputDto): Order = orderService.create(orderInput)
+  //
+  //  @ResponseStatus(HttpStatus.OK)
+  //  @PutMapping("{id}")
+  //  fun update(
+  //    @PathVariable id: UUID,
+  //    @RequestBody orderInput: OrderInputDto
+  //  ): Order = orderService.update(id, orderInput)
+  //
+  //  @ResponseStatus(HttpStatus.OK)
+  //  @PostMapping("search")
+  //  fun search(
+  //    @RequestBody param: SearchParam,
+  //    @RequestParam page: Number,
+  //    @RequestParam size: Number
+  //  ): Page<Order> = orderService.search(
+  //    param,
+  //    PageRequest.of(page.toInt(), size.toInt())
+  //  )
+  //
+  //  @ResponseStatus(HttpStatus.NO_CONTENT)
+  //  @DeleteMapping("{id}")
+  //  fun delete(@PathVariable id: UUID): Unit = orderService.delete(id)
 }
